80a264e340bb616a5394e4c83bae6121cac63bef
Николай Горичев <nikogorichev812@gmail.com>
2024-02-17 17:09:28 +0300

    add sidebar with search input

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -3,6 +3,7 @@ import styles from "./Cards.module.scss";
 const Cards = () => {
   return (
     <div className={styles.wrapper}>
+        
       <div className={styles.header}></div>
     </div>
   );
