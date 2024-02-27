07d12e049648eed39f3aa9bd8e0dfe60153f3457
Николай Горичев <nikogorichev812@gmail.com>
2024-02-27 11:40:31 +0300

    add useLocalStorage, add helpers getEntries

--- src/utils/dict/CategoryDict.tsx
+++ src/utils/dict/CategoryDict.tsx
@@ -1,13 +1,8 @@
 import { ReactComponent as IconBasket } from "assets/images/iconBasket.svg";
 import { ReactComponent as IconFavorite } from "assets/images/iconFavorite.svg";
 import { ReactComponent as IconAllList } from "assets/images/iconAllList.svg";
-import { SelectedCategoryType } from "utils/types/SelectedCategory";
 import { getEntries } from "utils/helpers/getEntries";
-
-type CategoryDict = Record<
-  SelectedCategoryType,
-  { title: string; icon: JSX.Element }
->;
+import { CategoryDict } from "utils/types/CategoryDict";
 
 const categoryDict: CategoryDict = {
   all: { title: "Весь список", icon: <IconAllList /> },
--- src/utils/helpers/getEntries.ts
+++ src/utils/helpers/getEntries.ts
@@ -1,6 +1,8 @@
+import { CategoryDict } from "utils/types/CategoryDict";
+
 type Entries<T> = {
   [K in keyof T]: [K, T[K]];
 }[keyof T][];
 
-export const getEntries = <T extends object>(obj: T) =>
+export const getEntries = <T extends CategoryDict>(obj: T) =>
   Object.entries(obj) as Entries<T>;
